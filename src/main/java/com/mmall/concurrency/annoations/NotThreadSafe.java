1a8a3eb0789daf8a77d7facdfaaf949c3cb969d4
huangm <ljaihm@qq.com>
2018-08-07 22:16:59 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *  标记【线程不安全】的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+	
+	String value() default "";
+	
+}
