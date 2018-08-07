6111664841c9b604dec93453f22577e4a5a55028
huangm <ljaihm@qq.com>
2018-08-07 21:49:28 +0800

    init

---
+++ src/main/java/com/mmall/concurrency/MainApplication.java
@@ -0,0 +1,16 @@
+package com.mmall.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * create 2018-08-07
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main( String[] args ){
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
